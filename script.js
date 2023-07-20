fa09364273c5456d1f52aa2896f9805fbeb87ed6
rcncln <53015856+rcncln@users.noreply.github.com>
2023-07-20 12:46:06 +0200

    deleted functions

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-const customSelects = document.querySelectorAll(".custom-select");
-
-// Function to toggle the options visibility
-function toggleOptions(select) {
-    const options = select.querySelector(".options");
-    options.classList.toggle("active");
-}
-
-// Function to set the selected option
-function setSelectedOption(select, option) {
-    const selectedOption = select.querySelector(".selected-option");
-    selectedOption.textContent = option.textContent;
-    toggleOptions(select);
-}
-
-
-
 window.addEventListener('click', () => {
     const customSelect = document.querySelector('.custom-select')
     customSelect.classList.toggle('active')
